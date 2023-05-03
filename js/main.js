1ea40028323bf6acf54b3dd4ae370e29aeda531c
gianella <g.cevallos@gianellin.com>
2023-05-03 14:50:26 -0400

    create variables js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const mainContainer = document.querySelector(".main-container")
+const afterContainer = document.querySelector(".after")
+
+
+const submitButton = document.getElementById("submit")
+const rateAgain = document.getElementById("again")
+
